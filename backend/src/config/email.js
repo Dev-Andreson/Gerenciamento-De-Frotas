1aa968c5bf436541539044dd01a7c5a7b1bddbcb
Dev-Andreson <dev.andreson@gmail.com>
2026-07-29 13:27:45 -0300

    Porta de configuração do email corrigida

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 587,
+  port: 465 ,
   secure: false, // false para porta 587
   auth: {
     user: process.env.EMAIL_USER,
